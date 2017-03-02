21da210ec674240eafd40578b55a6224ca89cced
Evan Bishop and Ryan McAlpin <ryan@>
2017-03-02 10:10:29 -0800

    create 9 space objects pushed to board array, prototype functions to get x and y coordinates

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,31 @@
 function Player(mark) {
-  this.mark = mark;
+  this.markCharacter = mark;
 }
+
+Player.prototype.mark = function() {
+  return this.markCharacter;
+};
+
+function Space(x, y) {
+  this.xCoordinate = x;
+  this.yCoordinate = y;
+}
+
+Space.prototype.getX = function () {
+  return this.xCoordinate;
+};
+Space.prototype.getY = function () {
+  return this.yCoordinate;
+};
+
+var board = [];
+for (var y = 0; y < 3; y++) {
+  for (var x = 0; x < 3; x++) {
+    var newSpace = new Space(x, y);
+    board.push(newSpace);
+  }
+}
+
 
 var playerX = new Player("X");
 var playerO = new Player("O");
