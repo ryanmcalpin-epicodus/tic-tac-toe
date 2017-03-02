08baf7d68b951590540287b42ab1477ffac58948
Evan Bishop and Ryan McAlpin <ryan@>
2017-03-02 14:54:49 -0800

    add user interface logic, error checking for clicking a space that is already occupied

--- js/scripts.js
+++ js/scripts.js
@@ -18,8 +18,13 @@ Space.prototype.getX = function () {
 Space.prototype.getY = function () {
   return this.yCoordinate;
 };
-Space.prototype.mark = function (whateverPlayer) {
-  this.markedBy = whateverPlayer.markCharacter;
+Space.prototype.mark = function (whateverPlayer, currentId) {
+  if (!this.markedBy) {
+    this.markedBy = whateverPlayer.markCharacter;
+    updateDisplay(this.markedBy, currentId);
+    checkWin();
+    switchTurn();
+  }
 };
 
 var board = [];
@@ -41,10 +46,19 @@ var switchTurn = function() {
 var checkWin = function() {
   var mark = players[turnIndex].markCharacter;
   if ((board[0].markedBy === mark && board[1].markedBy === mark && board[2].markedBy === mark) || (board[3].markedBy === mark && board[4].markedBy === mark && board[5].markedBy === mark) || (board[6].markedBy === mark && board[7].markedBy === mark && board[8].markedBy === mark) || (board[0].markedBy === mark && board[3].markedBy === mark && board[6].markedBy === mark) || (board[1].markedBy === mark && board[4].markedBy === mark && board[7].markedBy === mark) || (board[2].markedBy === mark && board[5].markedBy === mark && board[8].markedBy === mark) || (board[0].markedBy === mark && board[4].markedBy === mark && board[8].markedBy === mark) || (board[2].markedBy === mark && board[4].markedBy === mark && board[6].markedBy === mark)) {
-    return true;
+    gameOver();
   } else {
-    return false;
+    return;
   }
+}
+
+var convertId = function(eyeDee) {
+  var newId = eyeDee.slice(-1);
+  return newId;
+};
+
+var gameOver = function() {
+  alert("GAME OVER");
 }
 
 var turnIndex = 0;
@@ -52,3 +66,18 @@ var turnIndex = 0;
 var playerX = new Player("X");
 var playerO = new Player("O");
 var players = [playerX, playerO];
+
+
+
+var updateDisplay = function(mark, id) {
+  $("#" + id).text(players[turnIndex].markCharacter);
+}
+
+$(document).ready(function() {
+  $(".col-xs-4").click(function() {
+    var currentId = $(this).attr('id');
+    idNumber = convertId(currentId);
+    board[idNumber].mark(players[turnIndex], currentId);
+
+  });
+});
